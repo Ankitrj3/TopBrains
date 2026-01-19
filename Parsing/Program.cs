f40225eacf92fc5eb1973d5abe1ba6536f7cc043
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-19 11:56:22 +0530

    String Parsing

---
+++ Parsing/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Parsing
+{
+    public void ParseValue(string[] Tokens)
+    {
+        int sum = 0;
+        foreach(var token in Tokens)
+        {
+            if(int.TryParse(token, out int val))
+            {
+                sum += val;
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        string[] arr = {"12","nakir","jr","45","-5","456342327899999"};
+        Parsing p = new Parsing();
+        p.ParseValue(arr);
+    }
+}
