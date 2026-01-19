316cc6725a47ac21ecf12c806288f97c5c81b1d3
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-19 11:43:03 +0530

    Ref And Out Swap

---
+++ RefAndOutSwap/Program.cs
@@ -0,0 +1,38 @@
+﻿// Ref / Out – Swap Without Temp
+public class RefAndOutSwap
+{
+    public void RefSwap(ref int a, ref int b)
+    {
+        int x = a;
+        int y = b;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"Before {x} and {y} After {a} and {b}");
+    }
+    public void OutSwap(out int a, out int b)
+    {
+        a = 45;
+        b = 89;
+        int x = a;
+        int y = b;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"Before {x} and {y} After {a} and {b}");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        RefAndOutSwap refAndOutSwap = new RefAndOutSwap();
+        int a = 22;
+        int b = 13;
+        refAndOutSwap.RefSwap(ref a, ref b);
+        int c,y;
+        refAndOutSwap.OutSwap(out c, out y);
+    }
+}
