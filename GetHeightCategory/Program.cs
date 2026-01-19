528bf2d0fbc2489585fbe8c33da2a7cb9726e886
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-19 20:24:38 +0530

    Get Height Category

---
+++ GetHeightCategory/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static string GetHeightCategory(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(GetHeightCategory(140)); 
+        Console.WriteLine(GetHeightCategory(150)); 
+        Console.WriteLine(GetHeightCategory(179)); 
+        Console.WriteLine(GetHeightCategory(180)); 
+        Console.WriteLine(GetHeightCategory(250)); 
+    }
+}
