54612460e985475431dd51d0e2a198d6072d86c8
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-19 20:18:46 +0530

    Largest Among Three

---
+++ LargestOfThree/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static int LargestOfThree(int a, int b, int c)
+    {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(LargestOfThree(10, 5, 7));    
+        Console.WriteLine(LargestOfThree(-1, -5, -3)); 
+        Console.WriteLine(LargestOfThree(4, 4, 2));
+
+    }
+
+}
