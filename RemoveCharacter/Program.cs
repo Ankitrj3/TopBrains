1d3544b246815715a8b504b18d16c22ff6403ea2
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-27 11:43:08 +0530

    Remove Duplicates From String

---
+++ RemoveCharacter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void RemoveDuplicateChar(string str)
+    {
+        string result = "";
+        for(int i = 0; i < str.Length; i++)
+        {
+            if(i>0 && str[i] == str[i - 1])
+            {
+                continue;
+            }
+            if(i==0 || str[i-1]== ' ')
+            {
+                result+=char.ToUpper(str[i]);
+            }
+            else
+            {
+                result+=char.ToLower(str[i]);
+            }
+        }
+        Console.WriteLine(result);
+    }
+    public static void Main()
+    {
+        string? str = Console.ReadLine().Trim();
+        RemoveDuplicateChar(str);
+    }
+}
