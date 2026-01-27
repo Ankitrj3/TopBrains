71ddb32b3439f9d420a2e7db3b576b135869082c
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-27 21:26:11 +0530

    Count Character

---
+++ CountCharacter/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static int countChar(string str)
+    {
+        int count = 0;
+        str = str.ToLower();
+        foreach(char ch in str)
+        {
+            if(ch == ' ')
+            {
+                count--;
+            }
+            count++;
+        }
+        return count;
+    }
+    public static void Main()
+    {
+        string? val = "ankit ranjan";
+        Console.WriteLine("Count char of string "+countChar(val));
+    }
+}
