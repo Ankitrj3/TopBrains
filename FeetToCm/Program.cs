dec4e6eb6265fc0bfc7f6e6dc2c1dc3a78aa3b91
ankitrj3 <ankitrobinranjan@gmail.com>
2026-01-27 21:17:56 +0530

    Feet to Centimeter

---
+++ FeetToCm/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static double FeetToCm(int Feet)
+    {
+        double foot = 30.48;
+        double res = Feet * foot;
+        return res;
+    }
+    public static void Main()
+    {
+        int Feet = 4;
+        double result = FeetToCm(Feet);
+        Console.WriteLine($"Conversion of {Feet} into {result:F2} cm");
+    }
+}
